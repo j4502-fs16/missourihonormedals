5d233087ab462d3f6b4655525cb320f83dbc63d0
jiayi28 <jw778@mail.missouri.edu>
2016-11-14 14:27:46 -0600

    "hamburger menu"
    This reverts commit 668d4cb65d441e712ff50c8751f8f08da3e048fa.

--- javascript.js
+++ javascript.js
@@ -5,20 +5,4 @@ function showsub(li){
 function hidesub(li){
  var submenu=li.getElementsByTagName("ul")[0];
  submenu.style.display="none";
-}</span>
-
-
-//Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
-
-$(document).ready(function() {
-  $('.menu-trigger').click(function() {
-    $('#nav ul').slideToggle(500);
-  });//end slide toggle
-
-//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
-  $(window).resize(function() {
-	   if (  $(window).width() > 500 ) {
-		$('#nav ul').removeAttr('style');
-	 }
-	});//end resize
-});//end ready
+}</span> 
