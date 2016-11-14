03e2f7690a9184c50e05fdc2405d27cd9ffd2e25
jiayi28 <jw778@mail.missouri.edu>
2016-11-14 14:29:46 -0600

    js change

--- javascript.js
+++ javascript.js
@@ -5,4 +5,20 @@ function showsub(li){
 function hidesub(li){
  var submenu=li.getElementsByTagName("ul")[0];
  submenu.style.display="none";
-}</span> 
+}</span>
+
+
+//Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
+
+$(document).ready(function() {
+  $('parents').click(function() {
+    $('sub').slideToggle(500);
+  });//end slide toggle
+
+//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
+  $(window).resize(function() {
+	   if (  $(window).width() > 600 ) {
+		$('sub').removeAttr('style');
+	 }
+	});//end resize
+});//end ready
