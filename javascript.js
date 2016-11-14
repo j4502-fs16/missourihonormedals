1787e2caa637e5e80f56ebe13db77587e8b6198f
jiayi28 <jw778@mail.missouri.edu>
2016-11-14 15:33:30 -0600

    navigation

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
-//Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
+
+//desktop main menu
+$(document).ready(function() {
+  $('.parents-1').click(function() {
+    $('.sub-1').slideToggle(500);
+  });
+  $(window).resize(function() {
+       if (  $(window).width() > 600 ) {
+        $('.sub1').removeAttr('style');
+     }
+    });
+});
 
 $(document).ready(function() {
-  $('parents').click(function() {
-    $('sub').slideToggle(500);
-  });//end slide toggle
-
-//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
+  $('.parents-2').click(function() {
+    $('.sub-2').slideToggle(500);
+  });
   $(window).resize(function() {
-	   if (  $(window).width() > 600 ) {
-		$('sub').removeAttr('style');
-	 }
-	});//end resize
-});//end ready
+       if (  $(window).width() > 600 ) {
+        $('.sub1').removeAttr('style');
+     }
+    });
+});
+
+$(document).ready(function() {
+  $('.parents-3').click(function() {
+    $('.sub-3').slideToggle(500);
+  });
+  $(window).resize(function() {
+       if (  $(window).width() > 600 ) {
+        $('.sub1').removeAttr('style');
+     }
+    });
+});
