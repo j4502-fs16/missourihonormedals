a2119f44e7b5e890f4b217ee9c4e941ad4ac4060
jiayi28 <jw778@mail.missouri.edu>
2016-11-14 19:17:03 -0600

    78.88888% done hamburger menu

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 //desktop main menu
 $(document).ready(function() {
   $('.parents-1').click(function() {
@@ -32,3 +31,21 @@ $(document).ready(function() {
      }
     });
 });
+
+//hamburger menu
+
+
+//Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('.mainnav').slideToggle(500);
+  });//end slide toggle
+
+//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
+  $(window).resize(function() {
+	   if (  $(window).width() > 600 ) {
+		$('.mainnav').removeAttr('style');
+	 }
+	});//end resize
+});//end ready
